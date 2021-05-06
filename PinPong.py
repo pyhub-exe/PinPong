0e3d1103d8aa86753a742c0fe748af6265e5bba3
Nikita108768 <83420330+Nikita108768@users.noreply.github.com>
2021-05-06 23:45:31 +0300

    Update PinPong.py
    Версия 1.4 . Добавлены 2 ракетки которые управляются разными кнопками. Добавлена музыка. Добавлены стены и финиш игры но они пока что не работают.

--- PinPong.py
+++ PinPong.py
@@ -40,7 +40,51 @@ class Ball(Main):
                 self.nface="up" 
         self.reset()
 
-ball=Ball(x=200,y=200,filename="Ball.png",speed = 30,v = 50,n = 50)
+class Racketka(Main):
+    def update(self):
+        keys=key.get_pressed()
+        if keys[K_w]:
+            self.rect.y -= self.speed
+        if keys[K_s]:
+            self.rect.y += self.speed
+        self.reset()
+
+class Racketka1(Main):
+    def update(self):
+        keys=key.get_pressed()
+        if keys[K_p]:
+            self.rect.y -= self.speed
+        if keys[K_l]:
+            self.rect.y += self.speed
+        self.reset()
+
+class Wall(sprite.Sprite):  
+
+    def __init__(self,x,y,w,h,color):  
+        self.image = Surface((w,h))   
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def reset(self):
+        win.blit(self.image,( self.rect.x,self.rect.y))
+    def update(self):
+#        if sprite.collide_rect(self,ball):
+#            global end_game
+#            end_game = lose_text       
+        self.reset()
+
+ball = Ball( x= 200,y = 540,filename="Ball.png",speed = 10,v = 50,n = 50)
+racketka = Racketka(x = 10,y=540,filename="racketka1.png",speed = 10,v = 50,n = 160)
+racketka1 = Racketka1(x = 1860,y = 540,filename="racketka.png",speed = 10,v = 50,n = 160)
+
+CornflowerBlue = (100,149,237)   
+DarkSlateBlue = (72,61,139)  
+PeachPuff = (255,218,185)   
+
+side_stena = Wall(0,0,1,1080,PeachPuff)     
+side_stena1 = Wall(1919,0,1,1080,PeachPuff)   
 
 resolution = [w,h]
 
@@ -50,10 +94,18 @@ display.set_caption("ПинПонг")
 timer = time.Clock()
 FPS = 60
 
+mixer.init()
+mixer.music.load("Music.mp3")
+mixer.music.play()
+
 fon=image.load("fon.png")
 
-game = True
+font.init()
+shirift = font.SysFont("Arial",48)
+lose_text = shirift.render("ТЫ ПРОИГРАЛ",False,DarkSlateBlue)
 
+game = True
+end_game = False
 while game:
     for e in event.get():
         if e.type == 2:
@@ -62,9 +114,14 @@ while game:
                 game = False
 
     win.blit(fon,(0,0))
+    timer.tick(FPS)   
 
-    ball.update() 
-
-
-
+    if end_game == False:
+        ball.update() 
+        racketka.update()
+        racketka1.update()
+        side_stena.update()
+        side_stena1.update()
+    if end_game == lose_text:
+        win.blit(lose_text,(800,500))
     display.update()

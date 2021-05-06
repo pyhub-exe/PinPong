9b019f1fbb1d37b1649397a8952a750985ae46f2
Nikita108768 <83420330+Nikita108768@users.noreply.github.com>
2021-05-06 20:41:18 +0300

    Update PinPong.py
    Версия 1.1 с мячиком который летает с право налево

--- PinPong.py
+++ PinPong.py
@@ -1,8 +1,39 @@
 from pygame import *
 w=1920
-n=1080
+h=1080
 
-resolution = [w,n]
+class Main(sprite.Sprite):
+
+    def __init__(self,x,y,filename,speed,v,n):
+        self.image = image.load(filename)
+        self.image = transform.scale(self.image,(v,n))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.n = n
+        self.v = v
+        self.hface = "right"
+        self.nface = "up"
+        self.speed = speed
+
+    def reset(self):
+        win.blit(self.image,( self.rect.x,self.rect.y))
+
+class Ball(Main):
+    def update(self):
+        if self.hface == "right":
+            self.rect.x += self.speed
+            if self.rect.x + self.v > w:
+                self.hface="left"
+        else:
+            self.rect.x -= self.speed
+            if self.rect.x < 0:
+                self.hface="right" 
+        self.reset()
+
+ball=Ball(x=200,y=200,filename="Ball.png",speed = 30,v = 50,n = 50)
+
+resolution = [w,h]
 
 win = display.set_mode((resolution),flags = FULLSCREEN)
 display.set_caption("ПинПонг")
@@ -23,7 +54,7 @@ while game:
 
     win.blit(fon,(0,0))
 
-
+    ball.update() 
 
 
 

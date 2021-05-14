7299f4c89309b873b9c73eac80b7e623a1ec199b
Nikita108768 <83420330+Nikita108768@users.noreply.github.com>
2021-05-14 14:34:36 +0300

    Update PinPong.py
    Итоговая версия PinPong

--- PinPong.py
+++ PinPong.py
@@ -1,6 +1,9 @@
 from pygame import *
+from random import random
 w=1920
 h=1080
+q = 10
+a = 10
 
 class Main(sprite.Sprite):
 
@@ -20,16 +23,38 @@ class Main(sprite.Sprite):
         win.blit(self.image,( self.rect.x,self.rect.y))
 
 class Ball(Main):
+    def __init__(self,x,y,filename,speed,v,n):
+        super().__init__(x,y,filename,speed,v,n)
+        self.need_change_speed = False
+        self.last_touch_racket = False
+
+    def change_speed(self):
+        self.speed += 10
+        self.need_change_speed = True
+
     def update(self):
+        if self.need_change_speed:
+            self.speed -= 0.1
+            if self.speed < q:
+                self.speed = 10
+                self.need_change_speed = False
+
         if self.hface == "right":
             self.rect.x += self.speed
-            if self.rect.x + self.v > w:
+            if sprite.collide_rect(self,racketka_right):
+                self.last_touch_racket = "right"
+                self.change_speed()
                 self.hface="left"
+                if random() > 0.5:
+                    self.nface = "up"
         else:
             self.rect.x -= self.speed
-            if self.rect.x < 0:
+            if sprite.collide_rect(self,racketka_left):
+                self.last_touch_racket = "left"
+                self.change_speed()
                 self.hface="right" 
-        
+                if random() > 0.5:
+                    self.nface = "down" 
         if self.nface == "up":
             self.rect.y -= self.speed
             if self.rect.y < 0:
@@ -41,7 +66,25 @@ class Ball(Main):
         self.reset()
 
 class Racketka(Main):
+    def __init__(self,x,y,filename,speed,v,n):
+        super().__init__(x,y,filename,speed,v,n)
+        self.need_change_speed = False
+
+    def change_speed(self):
+        self.speed -= 5
+        self.need_change_speed = True
+
     def update(self):
+        if self.rect.y < 0:
+            self.rect.y = 0
+        if self.rect.y + 160 > h:
+            self.rect.y = h - 160
+        if self.need_change_speed:
+            self.speed += 0.01
+            if self.speed > a:
+                self.speed = a
+                self.need_change_speed = False
+
         keys=key.get_pressed()
         if keys[K_w]:
             self.rect.y -= self.speed
@@ -50,7 +93,26 @@ class Racketka(Main):
         self.reset()
 
 class Racketka1(Main):
+    def __init__(self,x,y,filename,speed,v,n):
+        super().__init__(x,y,filename,speed,v,n)
+        self.need_change_speed = False
+
+    def change_speed(self):
+        self.speed -= 5
+        self.need_change_speed = True
+
+    
     def update(self):
+        if self.rect.y < 0:
+            self.rect.y = 0
+        if self.rect.y + 160 > h:
+            self.rect.y = h - 160
+        if self.need_change_speed:
+            self.speed += 0.01
+            if self.speed > a:
+                self.speed = a
+                self.need_change_speed = False
+
         keys=key.get_pressed()
         if keys[K_p]:
             self.rect.y -= self.speed
@@ -70,14 +132,44 @@ class Wall(sprite.Sprite):
     def reset(self):
         win.blit(self.image,( self.rect.x,self.rect.y))
     def update(self):
-#        if sprite.collide_rect(self,ball):
-#            global end_game
-#            end_game = lose_text       
+        if sprite.collide_rect(self,ball):
+            global end_game
+            end_game = lose_text       
         self.reset()
 
-ball = Ball( x= 200,y = 540,filename="Ball.png",speed = 10,v = 50,n = 50)
-racketka = Racketka(x = 10,y=540,filename="racketka1.png",speed = 10,v = 50,n = 160)
-racketka1 = Racketka1(x = 1860,y = 540,filename="racketka.png",speed = 10,v = 50,n = 160)
+class Bird(Main):  
+    def update(self):
+        if self.hface == "right":
+            self.rect.x += self.speed
+            if self.rect.x + self.v > w:
+                self.hface="left"
+        else:
+            self.rect.x -= self.speed
+            if self.rect.x < 0:
+                self.hface="right" 
+        
+        if self.nface == "up":
+            self.rect.y -= self.speed
+            if self.rect.y < 0:
+                self.nface="down"
+        else:
+            self.rect.y += self.speed
+            if self.rect.y + self.n > h:
+                self.nface="up" 
+        if sprite.collide_rect(self,ball):
+            if ball.last_touch_racket == "right":
+                racketka_right.change_speed()
+            if ball.last_touch_racket == "left":
+                racketka_left.change_speed()
+                print("Ты попал")
+
+
+        self.reset()
+
+ball = Ball(x= 200,y = 540,filename="Ball.png",speed = q,v = 50,n = 50)
+racketka_left = Racketka(x = 10,y=540,filename="racketka1.png",speed = a,v = 50,n = 160)
+racketka_right = Racketka1(x = 1860,y = 540,filename="racketka.png",speed = a,v = 50,n = 160)
+ptica = Bird(x= 200,y = 200,filename="Bird.png",speed = 5,v = 100,n = 100)
 
 CornflowerBlue = (100,149,237)   
 DarkSlateBlue = (72,61,139)  
@@ -101,7 +193,7 @@ mixer.music.play()
 fon=image.load("fon.png")
 
 font.init()
-shirift = font.SysFont("Arial",48)
+shirift = font.SysFont("Impact",148)
 lose_text = shirift.render("ТЫ ПРОИГРАЛ",False,DarkSlateBlue)
 
 game = True
@@ -118,8 +210,9 @@ while game:
 
     if end_game == False:
         ball.update() 
-        racketka.update()
-        racketka1.update()
+        ptica.update() 
+        racketka_left.update()
+        racketka_right.update()
         side_stena.update()
         side_stena1.update()
     if end_game == lose_text:
